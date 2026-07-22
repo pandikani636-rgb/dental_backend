72c0165f9c5bd596385a46e541e8ec810ef689c5
pandikani <pandikani636@gmail.com>
2026-07-22 18:24:42 +0530

    commit

--- config/database.js
+++ config/database.js
@@ -9,7 +9,16 @@ const connectDatabase = async () => {
     }
 
     try {
-        const db = await mongoose.connect(process.env.MONGO_URI);
+        let uri = process.env.MONGO_URI;
+        if (uri) {
+            // Fix Vercel shell variable expansion removing '$Pk' from the password
+            if (uri.includes('pandikani636_db_user:877887@')) {
+                uri = uri.replace('pandikani636_db_user:877887@', 'pandikani636_db_user:877887%24Pk@');
+            } else if (uri.includes('pandikani636_db_user:877887$Pk@')) {
+                uri = uri.replace('pandikani636_db_user:877887$Pk@', 'pandikani636_db_user:877887%24Pk@');
+            }
+        }
+        const db = await mongoose.connect(uri);
         isConnected = db.connections[0].readyState;
         console.log("MongoDB Connected Successfully");
     } catch (error) {
