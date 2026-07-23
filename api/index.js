120fd5721036a9998ed094ea84985d49a84ab4b8
pandikani <pandikani636@gmail.com>
2026-07-23 13:20:45 +0530

    karanCommit

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const app = require("../app");
 const cloudinary = require("cloudinary");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
+  cloud_name: process.env.CLOUDINARY_NAME || process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ process.on('uncaughtException', (err) => {
 
 // Cloudinary configuration from environment variables
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
+    cloud_name: process.env.CLOUDINARY_NAME || process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
